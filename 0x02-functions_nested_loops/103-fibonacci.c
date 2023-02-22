5248cc2886f9c540d96a50d2127f2d15245faf66
my <myemail.com>
2023-02-21 19:46:01 -0800

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,8 +7,9 @@
  */
 
 int main(void)
-{
-	int i = 0;
+
+{	
+        int i = 0;
 	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
@@ -16,6 +17,7 @@ int main(void)
 		k += j;
 		if (k % 2 == 0)
 		sum += k;
+	        j = k - j;	
 		++i;
 	}
 	printf("%ld\n", sum);
